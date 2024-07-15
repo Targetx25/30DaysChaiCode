c89766a0d862e9e34be08174695d4b69e92ab603
TargetX25 <espinoza1250125@gmail.com>
2024-07-15 19:02:31 +0530

    day1 complete

---
+++ day1.js
@@ -0,0 +1,26 @@
+var number1 = 10;
+console.log(number1);
+let myName = "Mohit";
+console.log(myName);
+const trafficLight  = true;
+console.log(trafficLight);
+let fruits = ["Apple", "Banana", "Mango"];
+
+//Typeof of each 
+
+
+console.log(typeof(number1));
+console.log(typeof(myName));
+console.log(typeof(trafficLight));
+console.log(typeof(fruits));
+
+//Resassginig Variable
+let fruit = " Mango";
+console.log(fruit);
+fruit = "Apple";
+console.log(fruit);
+
+//Reassigning the const
+
+// trafficLight = false;
+
