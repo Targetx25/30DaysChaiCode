dfb9d1995ddd50dc091266c580587868d8f9fe5a
TargetX25 <espinoza1250125@gmail.com>
2024-07-17 00:22:23 +0530

    day4-complete

---
+++ day4.js
@@ -0,0 +1,87 @@
+//Taks1
+
+for(let i =1; i <=10 ; i++){
+     console.log(i)
+}
+
+//Task 2
+for(let i = 1 ; i <=10 ; i++){
+     console.log(5*i)
+}
+
+//TAsk 3
+
+let sum = 0;
+
+let i = 1;
+
+while(i <= 10){
+     sum = sum + i;
+     i++;
+    
+}
+console.log(sum)
+
+//Task 4
+let x =10 ;
+while(x >0){
+     console.log(x)
+     x--;
+}
+
+//tAsk five
+
+
+let z = 1
+do {
+     console.log(z)
+     z++;
+} while (z < 6);
+
+//task6 
+
+let factorial = 1;
+let number = 6;
+
+do {
+     factorial *= number;
+     number--;
+} while (number > 0);
+
+console.log(factorial)
+
+
+//task7 
+
+for(i = 1; i <=5; i++){
+     let pattern = "*"
+     console.log(pattern.repeat(i))
+}
+
+
+
+//task8
+for(i = 1; i <=10; i++){
+     if(i ==5){
+          continue;
+
+     }
+
+     console.log(i)
+}
+
+//task 9
+for (let index = 0; index < 10; index++) {
+     
+     if(index == 7){
+          break;
+     }
+     console.log(index)
+}
+
+
+
+
+
+
+
